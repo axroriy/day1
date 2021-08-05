9b07c8de382ce6dc0a5af6f89632e683a304b8f7
Axrorbek <87981374+axroriy@users.noreply.github.com>
2021-08-05 17:52:11 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let money = prompt("pulni kiriting");
+let dollor = 10654;
+let yevro = 12674;
+let natija = `Sizning pulingiz : ${Math.floor(money/dollor) } dollor yoki ${money/dollor} dollor   
+            ${Math.floor(money/yevro) } yevro yoki ${money/yevro} yevro `;
+alert(natija);  
